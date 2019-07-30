3b4230e0067b06ce7a8bb3e2f106d848843e0b97
Taylor Wang <vic1997wang@gmail.com>
2019-07-30 16:44:11 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <div class="tag" @click="skip">flex-demo（父容器justify-content, align-items, align-content）</div>
+        <span>2019年7月30日</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods: {
+      skip: function () {
+        this.$router.push({path: '/flex'})
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    width: 75%;
+    margin-left: auto;
+    margin-right: auto;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    width: 100%;
+    display: inline-block;
+    margin: 0 10px;
+    border-bottom: 10px;
+    height: 30px;
+  }
+
+  .tag {
+    float: left;
+    color: #42b983;
+    font-size: 25px;
+  }
+
+  .tag:hover {
+    color: red;
+  }
+
+  span {
+    float: right;
+    font-size: 25px;
+  }
+</style>
---
+++ src/pages/flex/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <span>flex父容器部分属性演示</span>
+    <div class="div1">
+      <ul class="ul1" :style="styleObject">
+        <div class="ulcontain" v-for="(item,index) in [1,2,3,4,5,6,7]" :key="index">
+          <li>this is test {{item}}</li>
+        </div>
+      </ul>
+      <div>
+        <span>父容器justify-content属性演示</span>
+        <ul class="ul2">
+          <label class="myradio" v-for="(item,index) in justifyContentList">
+            <input class="radio-icon" type="radio" name="justifyContent" :value="item" v-model="justifyContentpicked"
+                   :key="index">
+            <span class="radio-content">{{item}}</span>
+          </label>
+        </ul>
+        <span>Picked: {{ justifyContentpicked }}</span>
+      </div>
+      <div>
+        <span>父容器align-items属性演示</span>
+        <ul class="ul2">
+          <label class="myradio" v-for="(item,index) in alignItemsList">
+            <input class="radio-icon" type="radio" name="alignItems" :value="item" v-model="alignItemspicked"
+                   :key="index">
+            <span class="radio-content">{{item}}</span>
+          </label>
+        </ul>
+        <span>Picked: {{ alignItemspicked }}</span>
+      </div>
+      <div>
+        <span>父容器align-content属性演示</span>
+        <ul class="ul2">
+          <label class="myradio" v-for="(item,index) in alignContentList">
+            <input class="radio-icon" type="radio" name="alignContent" :value="item" v-model="alignContentpicked"
+                   :key="index">
+            <span class="radio-content">{{item}}</span>
+          </label>
+        </ul>
+        <span>Picked: {{ alignContentpicked }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    data() {
+      return {
+        justifyContentpicked: "",
+        alignItemspicked: "",
+        alignContentpicked: "",
+        styleObject: {
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
+        },
+        justifyContentList: ["flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly"],
+        alignItemsList: ["stretch", "flex-start", "flex-end", "center", "baseline"],
+        alignContentList: ["stretch", "flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly"]
+      }
+    },
+    methods: {},
+    watch: {
+      justifyContentpicked: function () {
+        this.styleObject.justifyContent = this.justifyContentpicked;
+      },
+      alignItemspicked: function () {
+        this.styleObject.alignItems = this.alignItemspicked;
+      },
+      alignContentpicked: function () {
+        this.styleObject.alignContent = this.alignContentpicked;
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  @a: 5px;
+  @import "flex.less";
+</style>
