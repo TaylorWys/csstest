3a426a9856854767ede1bccc299114e1812ed320
Taylor Wang <vic1997wang@gmail.com>
2019-07-31 10:54:00 +0800

    Add flexChild

--- src/pages/flexChild/index.vue
+++ src/pages/flexChild/index.vue
@@ -1,13 +1,50 @@
 <template>
   <div>
     <span>flex子组件属性演示</span>
-    <p>注：flex容器已设置align-items:center, justify-content:space-evenly</p>
+    <p>注①：flex容器已设置align-items:center, justify-content:space-evenly</p>
+    <p>注②：各项第一个选项为默认值</p>
     <div class="div1">
-      <div class="ul1" :style="styleObject">
-        <div class="licontain" v-for="(item,index) in [1,2,3]" :key="index">
-          <span>this is test {{item}}</span>
+      <div class="ul1">
+        <div class="licontain">
+          <span>this is test 1</span>
+        </div>
+        <div class="licontain" :style="styleObject">
+          <span>this is test 2</span>
+        </div>
+        <div class="licontain">
+          <span>this is test 3</span>
         </div>
       </div>
+    </div>
+    <div class="radio-container">
+      <span>order属性：</span>
+      <ul class="ul2">
+        <label class="my-radio" v-for="(item,index) in orderList">
+          <input class="radio-icon" type="radio" name="orderList" :value="item" v-model="orderPicked"
+                 :key="index">
+          <span class="radio-content">{{item}}</span>
+        </label>
+      </ul>
+    </div>
+    <div class="radio-container">
+      <span>flex-grow属性：</span>
+      <ul class="ul2">
+        <label class="my-radio" v-for="(item,index) in flexGrowList">
+          <input class="radio-icon" type="radio" name="flexGrowList" :value="item" v-model="flexGrowPicked"
+                 :key="index">
+          <span class="radio-content">{{item}}</span>
+        </label>
+      </ul>
+    </div>
+    <div class="radio-container">
+      <span>align-self属性：</span>
+      <ul class="ul2">
+        <label class="my-radio" v-for="(item,index) in alignSelfList">
+          <input class="radio-icon" type="radio" name="alignSelfList" :value="item" v-model="alignSelfPicked"
+                 :key="index">
+          <span class="radio-content">{{item}}</span>
+        </label>
+      </ul>
     </div>
   </div>
 </template>
@@ -15,15 +52,33 @@
 <script>
   export default {
     name: "index",
-    data () {
-      return{
+    data() {
+      return {
+        orderList: [-1, 0, 1],
+        orderPicked: 0,
+        flexGrowList: [0, 0.25, 0.5, 1],
+        flexGrowPicked: 0,
+        alignSelfList:["auto","stretch", "flex-start", "flex-end", "center", "baseline"],
+        alignSelfPicked:"auto",
+        styleObject: {
+          color: "blue",
+          order: 0,
+          flexGrow: 0,
+          alignSelf: "auto"
+        }
       }
     },
-    methods:{
-
-    },
-    watch:{
-
+    methods: {},
+    watch: {
+      orderPicked: function () {
+        this.styleObject.order = this.orderPicked
+      },
+      flexGrowPicked: function () {
+        this.styleObject.flexGrow = this.flexGrowPicked
+      },
+      alignSelfPicked: function () {
+        this.styleObject.alignSelf = this.alignSelfPicked
+      }
     }
   }
 </script>
